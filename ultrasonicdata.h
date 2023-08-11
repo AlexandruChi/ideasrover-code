d459997ac034606c9fe8944648f912f4f9415c8b
Alexandru Chiriac <alexandruchi203@icloud.com>
2023-08-11 12:12:22 +0300

    fix

--- ultrasonicdata.h
+++ ultrasonicdata.h
@@ -2,6 +2,7 @@
 #define __ultrasonicdata_h
 
 // ultasonic data used by the self driving program
+// can be replace with something diferent
 struct UltrasonicData {
     // ultrasonic data goes here
 };
