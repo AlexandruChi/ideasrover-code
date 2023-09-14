93a11bc0797adfeb208505d2869f0f3e5e580205
Alexandru Chiriac <alexandruchi203@icloud.com>
2023-09-14 14:57:29 +0300

    rover main program

--- rover/main.c
+++ rover/main.c
@@ -1,11 +1,13 @@
 // TODO header file with the component path and ports
 
+#include <unistd.h>
+
 #include "../connection/connection.h"
 
 #define and &&
 #define or ||
 
-#define loop while (1)
+#define loop for(;;)
 
 #define wait_while(X) while (X) {}
 #define wait_while_not(X) wait_while(!(X))
@@ -15,15 +17,15 @@
 
 // TODO make better data types
 
-struct objectDetectionCameraData {
+struct objectData {
     // insert something here or replace struct
 };
 
-struct PixyCameraData {
+struct laneData {
     // insert something here or replace struct
 };
 
-struct batteryManagementSystemData {
+struct batteryData {
     unsigned char batteryProcent;
     bool charging;
 };
@@ -36,12 +38,13 @@ struct ESCData {
 };
 
 struct selfDrivingData {
+    bool running; // replace with something better
     // insert something here
 };
 
 struct roverControll {
     struct ESCData esc;
-    double speed;
+    double steer;
 };
 
 struct roverData {
@@ -54,6 +57,8 @@ struct roverParameters {
     bool test;
 };
 
+void runComponentExecutable(char* path);
+
 int main() {
 
     // input devices
@@ -64,9 +69,9 @@ int main() {
     Connection phoneInput, phoneOutput, selfDrvingInput, selfDrivingOutput;
 
     double distance;
-    struct objectDetectionCameraData objectDetectionCameraData;
-    struct PixyCameraData PixyCameraData;
-    struct batteryManagementSystemData batteryManagementSystemData;
+    struct objectData objectData;
+    struct laneData laneData;
+    struct batteryData batteryData;
 
     struct ESCData ESCData;
     double steer;
@@ -79,9 +84,9 @@ int main() {
 
     // creating server for input devices
     ultrasonicSensor = createLocalConnection("/tmp/ultrasonicsensor", true, true, sizeof(distance));
-    objectDetectionCamera = createLocalConnection("/tmp/objectdetectioncamera", true, true, sizeof(objectDetectionCameraData));
-    PixyCamera = createLocalConnection("/tmp/pixycamera", true, true, sizeof(PixyCameraData));
-    batteryManagementSystem = createLocalConnection("tmp/batterymanagementsystem", true, true, sizeof(batteryManagementSystemData));
+    objectDetectionCamera = createLocalConnection("/tmp/objectdetectioncamera", true, true, sizeof(objectData));
+    PixyCamera = createLocalConnection("/tmp/pixycamera", true, true, sizeof(laneData));
+    batteryManagementSystem = createLocalConnection("tmp/batterymanagementsystem", true, true, sizeof(batteryData));
 
     // creating server for output devices
     ESC = createLocalConnection("/tmp/esc", false, true, sizeof(ESCData));
@@ -91,13 +96,10 @@ int main() {
     selfDrvingInput = createLocalConnection("/tmp/selfdrivinginput", true, true, sizeof(selfDrivingData));
     selfDrivingOutput = createLocalConnection("/tmp/selfdrivingoutput", false, true, sizeof(roverControll));
 
-    phoneInput = createNetworkConnection("", PHONE_INPUT_PORT, true, true, sizeof(roverParameters));
-    phoneOutput = createNetworkConnection("", PHONE_OUTPUT_PORT, false, true, sizeof(roverData));
+    // run the processes for components
+    runComponentExecutable(""); // TODO replace with stuff
 
-
-    
-
-
+    // wait for local sockets to create
     wait_while_not (
             isConnected(ultrasonicSensor) and
             isConnected(objectDetectionCamera) and
@@ -106,15 +108,57 @@ int main() {
             isConnected(ESC) and
             isConnected(servo) and
             isConnected(selfDrvingInput) and
-            isConnected(selfDrivingOutput) and
-            isConnected(phoneInput) and
-            isConnected(phoneOutput)
+            isConnected(selfDrivingOutput)
     )
 
+    // create server for phone
+    // maybe add script to create network
+    phoneInput = createNetworkConnection("", PHONE_INPUT_PORT, true, true, sizeof(roverParameters));
+    phoneOutput = createNetworkConnection("", PHONE_OUTPUT_PORT, false, true, sizeof(roverData));
+
+    // wait for phone to connect to rover
+    wait_while_not (isConnected(phoneInput) and isConnected(phoneOutput))
+
+    // TODO make better
+    // TODO make threads
+    // TODO fix errors
+    // TODO maybe check data before using it
     loop {
+        // get parameters and send data to phone
+        getConnectionData(phoneInput, &roverParameters);
+        setConnectionData(phoneOutput, &roverData);
+
+        // get data from sensors and cameras
+        getConnectionData(ultrasonicSensor, &distance);
+        getConnectionData(objectDetectionCamera, &objectData);
+        getConnectionData(PixyCamera, &laneData);
+        getConnectionData(batteryManagementSystem, &batteryData);
+
+        // insert code here for data procesing
+        // selfDrivingData = something usefull
+
+        // check if it should run first
+
+        // send and recive data to selfdriving program
+        setConnectionData(selfDrvingInput, &selfDrivingData);
+        getConnectionData(selfDrivingOutput, &roverControll);
+
+        // send data to servo and ESC
+        steer = roverControll.steer;
+        ESCData = roverControll.esc;
         
+        setConnectionData(servo, &steer);
+        setConnectionData(ESC, &ESCData);
     }
 
     // find a way to stop the program when powering off the PI
     return 0;
+}
+
+void runComponentExecutable(char* path) {
+    if (!fork()) {
+        for(;;) {
+            execl(path, path, NULL);
+        }
+    }
 }
